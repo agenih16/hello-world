cd6268ced307f4686ad99c57f6866406202c4fe1
agenih16 <agenih16@163.com>
2017-11-21 22:28:31 +0800

    sum
    add a sum function
    
    Signed-off-by: agenih16 <agenih16@163.com>

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#includes "iostream"
+#includes "string.h"
+
+using namespace std;
+
+void main()
+{
+	for(int i = 0; i<10; i++){
+		cout << i << endl;
+	}
+
+	int sum = 0;
+	for(i=0; i<200; i++){
+		sum += i;
+	}
+	cout << sum << endl;
+	cout << "hello world!" << endl;
+	return ;
+}
